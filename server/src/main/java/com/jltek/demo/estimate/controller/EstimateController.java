fb1402dea9e0dacf9ad8037e8659f0840ccf3aeb
yongwon jo <joyw93@naver.com>
2023-09-30 17:18:10 +0900

    feat: docker test

--- server/src/main/java/com/jltek/demo/estimate/controller/EstimateController.java
+++ server/src/main/java/com/jltek/demo/estimate/controller/EstimateController.java
@@ -5,12 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,7 +15,7 @@ public class EstimateController {
 
     @PostMapping()
     public ResponseEntity<String> getExample() {
-        String response = "Hello, World!";
+        String response = "docker test";
         return ResponseEntity.ok(response);
     }
 }
