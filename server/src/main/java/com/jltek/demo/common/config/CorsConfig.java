6d291c7a608a63bc9758734a4bcdb9485e544a4b
yongwon jo <joyw93@naver.com>
2023-10-05 23:27:13 +0900

    feat: add cors origin url

--- server/src/main/java/com/jltek/demo/common/config/CorsConfig.java
+++ server/src/main/java/com/jltek/demo/common/config/CorsConfig.java
@@ -1,2 +1,23 @@
-package com.jltek.demo.common.config;public class CorsConfig {
+package com.jltek.demo.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // 허용할 엔드포인트 패턴을 지정합니다.
+                .allowedOrigins(allowedOrigin) // 허용할 오리진을 지정합니다.
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // 허용할 HTTP 메서드를 지정합니다.
+    }
 }
+
