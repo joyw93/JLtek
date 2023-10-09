b0d84e5e13ef066d3ca2899fb3b23f1852878d7a
yongwon jo <joyw93@naver.com>
2023-10-09 15:19:09 +0900

    feat: cors test

--- server/src/main/java/com/jltek/demo/estimate/controller/EstimateController.java
+++ server/src/main/java/com/jltek/demo/estimate/controller/EstimateController.java
@@ -2,6 +2,7 @@ package com.jltek.demo.estimate.controller;
 
 import com.jltek.demo.estimate.service.EstimateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/v1/estimate")
 public class EstimateController {
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
     private final EstimateService estimateService;
 
     @PostMapping()
     public ResponseEntity<String> getExample() {
+        System.out.println(allowedOrigin);
         String response = "docker test 5";
         return ResponseEntity.ok(response);
     }
